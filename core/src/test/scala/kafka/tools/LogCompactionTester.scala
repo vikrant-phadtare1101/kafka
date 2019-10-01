019fdbed55d85f2b5db72f6790536f03ec7f2e2b
John Doe <john@sample.com>
2019-10-01 09:42:50 +0530

    commitMessage : MINOR: Use Admin Interface instead of AdminClient in Kafka tools.

--- core/src/test/scala/kafka/tools/LogCompactionTester.scala
+++ core/src/test/scala/kafka/tools/LogCompactionTester.scala
@@ -58,13 +58,12 @@ object LogCompactionTester {
   //maximum line size while reading produced/consumed record text file
   private val ReadAheadLimit = 4906
 
-  def main(args: Array[String]) {
+  def main(args: Array[String]): Unit = {
     val parser = new OptionParser(false)
-    val numMessagesOpt = parser.accepts("messages", "The number of messages to send or consume.")
+    val numMessagesOpt = parser.accepts("messages", "The number of messages to send or consume per topic.")
       .withRequiredArg
       .describedAs("count")
       .ofType(classOf[java.lang.Long])
-      .defaultsTo(Long.MaxValue)
     val messageCompressionOpt = parser.accepts("compression-type", "message compression type")
       .withOptionalArg
       .describedAs("compressionType")
@@ -89,9 +88,9 @@ object LogCompactionTester {
       .describedAs("percent")
       .ofType(classOf[java.lang.Integer])
       .defaultsTo(0)
-    val sleepSecsOpt = parser.accepts("sleep", "Time in milliseconds to sleep between production and consumption.")
+    val sleepSecsOpt = parser.accepts("sleep", "Time in seconds to sleep between production and consumption.")
       .withRequiredArg
-      .describedAs("ms")
+      .describedAs("secs")
       .ofType(classOf[java.lang.Integer])
       .defaultsTo(0)
 
@@ -115,7 +114,7 @@ object LogCompactionTester {
     val topics = (0 until topicCount).map("log-cleaner-test-" + testId + "-" + _).toArray
     createTopics(brokerUrl, topics.toSeq)
 
-    println(s"Producing $messages messages..to topics ${topics.mkString(",")}")
+    println(s"Producing ${messages * topicCount} messages..to topics ${topics.mkString(",")}")
     val producedDataFilePath = produceMessages(brokerUrl, topics, messages, compressionType, dups, percentDeletes)
     println(s"Sleeping for $sleepSecs seconds...")
     Thread.sleep(sleepSecs * 1000)
@@ -138,7 +137,7 @@ object LogCompactionTester {
   def createTopics(brokerUrl: String, topics: Seq[String]): Unit = {
     val adminConfig = new Properties
     adminConfig.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, brokerUrl)
-    val adminClient = admin.AdminClient.create(adminConfig)
+    val adminClient = admin.Admin.create(adminConfig)
 
     try {
       val topicConfigs = Map(TopicConfig.CLEANUP_POLICY_CONFIG -> TopicConfig.CLEANUP_POLICY_COMPACT)
@@ -157,7 +156,7 @@ object LogCompactionTester {
 
   def lineCount(filPath: Path): Int = Files.readAllLines(filPath).size
 
-  def validateOutput(producedDataFile: File, consumedDataFile: File) {
+  def validateOutput(producedDataFile: File, consumedDataFile: File): Unit = {
     val producedReader = externalSort(producedDataFile)
     val consumedReader = externalSort(consumedDataFile)
     val produced = valuesIterator(producedReader)
@@ -192,7 +191,7 @@ object LogCompactionTester {
     Utils.delete(consumedDedupedFile)
   }
 
-  def require(requirement: Boolean, message: => Any) {
+  def require(requirement: Boolean, message: => Any): Unit = {
     if (!requirement) {
       System.err.println(s"Data validation failed : $message")
       Exit.exit(1)
@@ -242,7 +241,7 @@ object LogCompactionTester {
     val builder = new ProcessBuilder("sort", "--key=1,2", "--stable", "--buffer-size=20%", "--temporary-directory=" + Files.createTempDirectory("log_compaction_test"), file.getAbsolutePath)
     val process = builder.start
     new Thread() {
-      override def run() {
+      override def run(): Unit = {
         val exitCode = process.waitFor()
         if (exitCode != 0) {
           System.err.println("Process exited abnormally.")
@@ -269,16 +268,20 @@ object LogCompactionTester {
     try {
       val rand = new Random(1)
       val keyCount = (messages / dups).toInt
+      val numMessages = messages * topics.length
+      var numDeleteMsgs = (numMessages * percentDeletes) / 100
       val producedFilePath = Files.createTempFile("kafka-log-cleaner-produced-", ".txt")
       println(s"Logging produce requests to $producedFilePath")
       val producedWriter: BufferedWriter = Files.newBufferedWriter(producedFilePath, UTF_8)
-      for (i <- 0L until (messages * topics.length)) {
+      for (i <- 0L until numMessages) {
         val topic = topics((i % topics.length).toInt)
         val key = rand.nextInt(keyCount)
-        val delete = (i % 100) < percentDeletes
+        val delete = numDeleteMsgs != 0 && (numDeleteMsgs == numMessages - i || rand.nextBoolean())
         val msg =
-          if (delete)
+          if (delete) {
+            numDeleteMsgs -= 1
             new ProducerRecord[Array[Byte], Array[Byte]](topic, key.toString.getBytes(UTF_8), null)
+          }
           else
             new ProducerRecord(topic, key.toString.getBytes(UTF_8), i.toString.getBytes(UTF_8))
         producer.send(msg)
